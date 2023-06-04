e24da03c83e893d66d73e210ea1da3b3f1a48bc6
JimmyHernandez <jimoem24@gmail.com>
2023-06-04 10:22:05 -0400

    first commit

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask application.
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_holberton():
+    """
+    Returns 'Hello HBNB!'.
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display():
+    """ Return display "HBNB" """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_c(text):
+    """ Display C and change _ to spaces"""
+    txt = text.replace("_", " ")
+    return "C " + txt
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
