0c3f93812ad0738c06e45cfa743d823b69c37c0e
JimmyHernandez <jimoem24@gmail.com>
2023-06-04 13:18:34 -0400

    first commit

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -48,7 +48,8 @@ def number_template(n):
     """ display n is a number"""
     return render_template("5-number.html", n=n)
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd(n):
     """ display n is a number"""
     return render_template("6-number_odd_or_even.html", n=n)
