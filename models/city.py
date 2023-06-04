726585c8013a6824f170e15407751f31968c7cfb
JimmyHernandez <jimoem24@gmail.com>
2023-06-04 09:31:11 -0400

    Add repository from Abigail to Jimmy

---
+++ models/city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" City Module for HBNB project """
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(BaseModel, Base):
+    """This is a Python class that inherits from two other classes/
+       BaseModel and Base, and represents a
+       city."""
+
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship(
+        'Place', cascade='all, delete-orphan', backref='city')
