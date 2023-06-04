6b746f20855b2f21951874b4256f5e49bebbc3b6
JimmyHernandez <jimoem24@gmail.com>
2023-06-04 10:08:54 -0400

    fix pep8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -15,10 +15,12 @@ def hello_holberton():
     """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb")
 def display():
     """ Return display "HBNB" """
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
